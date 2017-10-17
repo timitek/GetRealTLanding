e0d96d860d17a653dbd91c1aa0cb90352462ea35
Tavleen Kaur <tavleen@timitek.com>
2017-10-17 17:12:02 -0500

    Contact form

---
+++ resources/views/welcome/partials/contact.blade.php
@@ -0,0 +1,78 @@
+<div class="field">
+  <label class="label">Name</label>
+  <div class="columns is-mobile">
+        <div class="column"><input class="input is-primary" type="first_name" required placeholder="First Name"></div>
+        <div class="column"><input class="input is-primary" type="last_name" required placeholder="Last Name"></div>
+  </div>
+</div>
+
+<div class="field">
+<label class="label">Email</label>
+<div class="control has-icons-left has-icons-right">
+  <input class="input is-primary" type="email" placeholder="example@admin.com">
+  <span class="icon is-small is-left">
+    <i class="fa fa-envelope"></i>
+  </span>
+  <span class="icon is-small is-right">
+    <i class="fa fa-warning"></i>
+  </span>
+<!--</div>
+<p class="help is-danger">This email is invalid</p>
+</div>-->
+
+<div class="field">
+<label class="label">Phone</label>
+<div class="control has-icons-left has-icons-right">
+  <input class="input is-primary" type="tel" placeholder="###-###-####">
+  <span class="icon is-small is-left">
+    <i class="fa fa-phone"></i>
+  </span>
+</div>
+
+<div class="field">
+  <label class="label">Address</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Street Address">
+    <span class="icon is-small is-left">
+        <i class="fa fa-address-book-o" aria-hidden="true"></i>
+    </span>
+  </div>
+</div>
+<div class="field">
+  <label class="label">City</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="City Name">
+  </div>
+</div>
+<div class="row">
+    <label class="label">State</label>
+        <div class="control">
+            <div class="select">
+            <select>
+                <option>Arkansas</option>
+                <option>Texas</option>
+                <option>Missouri</option>
+                <option>Georgia</option>
+            </select>
+            </div>
+        <div>
+       
+    <label class="label">Zip</label>
+    <input class="input is-primary" type="number" required placeholder="Zip code">
+</div>
+
+<div class="field">
+    <p>Buy/transfer a domain and choose a hosting plan</p>
+</div>
+
+<div class="columns">
+  <div class="column">
+    <label class="label">State</label>
+  </div>
+  <div class="column">
+  <label class="label">GetRealT(Brokerage Bundle)</label>
+  </div>
+  <div class="column">
+  <label class="label">GetRealT(Brokerage Bundle)</label>
+  </div>
+</div>
---
+++ resources/views/welcome/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="title">
+Hello World
+</h1>
+<p class="subtitle">
+Get started with <strong>GetRealT</strong>!
+</p>
+
+  @include('welcome.partials.contact')
+@endsection
