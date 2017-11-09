7ca1797216258eb36282ab939c7f93357f930423
Tavleen Kaur <tavleen@timitek.com>
2017-11-09 00:52:02 -0600

    nothing

--- resources/views/welcome/partials/contact.blade.php
+++ resources/views/welcome/partials/contact.blade.php
@@ -1,3 +1,43 @@
+<div class="steps">
+  <div class="step-item is-completed is-success">
+    <div class="step-marker">
+      <span class="icon">
+        <i class="fa fa-check"></i>
+      </span>
+    </div>
+    <div class="step-details">
+      <p class="step-title">Step 1</p>
+      <p>This is the first step of the process.</p>
+    </div>
+  </div>
+  <div class="step-item is-active">
+    <div class="step-marker"></div>
+    <div class="step-details">
+      <p class="step-title">Step 2</p>
+      <p>This is the second step. You get here once you have completed the first step.</p>
+    </div>
+  </div>
+  <div class="step-item">
+    <div class="step-marker">3</div>
+    <div class="step-details">
+      <p class="step-title">Step 3</p>
+      <p>This is the third step. One more last before the end.</p>
+    </div>
+  </div>
+  <div class="step-item">
+    <div class="step-marker">
+      <span class="icon">
+        <i class="fa fa-flag"></i>
+      </span>
+    </div>
+    <div class="step-details">
+      <p class="step-title">Step 4</p>
+      <p>Final step. You have completed all the previous steps and end the process.</p>
+    </div>
+  </div>
+</div>
+
+
 <h1 class="title">
 Hello World
 </h1>
